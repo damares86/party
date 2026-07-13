f5070e98432039cf431b980019bd3130b3a6a144
damares86 <davidemasera@gmail.com>
2026-07-13 16:45:33 +0200

    update db backup

--- party.sql
+++ party.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Lug 09, 2026 alle 15:06
+-- Creato il: Lug 13, 2026 alle 16:45
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
@@ -32,19 +32,9 @@ CREATE TABLE `orders` (
   `email` varchar(255) NOT NULL,
   `place` varchar(255) DEFAULT NULL,
   `order_number` int(10) NOT NULL,
+  `bill` decimal(6,2) DEFAULT NULL,
   `paid` int(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dump dei dati per la tabella `orders`
---
-
-INSERT INTO `orders` (`id`, `email`, `place`, `order_number`, `paid`) VALUES
-(25, 'economo@agnelli.it', NULL, 1, 0),
-(26, 'economo@agnelli.it', NULL, 2, 0),
-(27, 'economo@agnelli.it', NULL, 3, 0),
-(28, 'economo@agnelli.it', NULL, 4, 0),
-(29, 'economo@agnelli.it', NULL, 5, 0);
 
 -- --------------------------------------------------------
 
@@ -60,24 +50,6 @@ CREATE TABLE `orders_details` (
   `qty` int(5) NOT NULL,
   `used` int(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dump dei dati per la tabella `orders_details`
---
-
-INSERT INTO `orders_details` (`id`, `orders_id`, `products_id`, `product_letter`, `qty`, `used`) VALUES
-(25, 25, 1, 'Z', 4, 0),
-(26, 25, 2, 'C', 3, 0),
-(27, 26, 1, 'Z', 4, 0),
-(28, 26, 2, 'D', 3, 0),
-(29, 27, 1, 'H', 4, 0),
-(30, 27, 2, 'S', 3, 0),
-(31, 27, 1, 'I', 1, 0),
-(32, 28, 1, 'J', 4, 0),
-(33, 28, 2, 'M', 5, 0),
-(34, 29, 1, 'X', 4, 0),
-(35, 29, 2, 'C', 4, 0),
-(36, 29, 1, 'D', 2, 0);
 
 -- --------------------------------------------------------
 
@@ -130,13 +102,13 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT per la tabella `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT per la tabella `orders_details`
 --
 ALTER TABLE `orders_details`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT per la tabella `products`
